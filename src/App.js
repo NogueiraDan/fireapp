2d04860b46428c861ff84b43d672e2f46a7f2406
Daniel Nogueira <silvanogueira3460@gmail.com>
2023-01-10 09:12:32 -0300

    Adição e busca

--- src/App.js
+++ src/App.js
@@ -1,22 +1,77 @@
 import "./App.css";
-import { useState } from "react";
+import { cloneElement, useState } from "react";
 import { db } from "./database.config";
-import { doc, setDoc } from "firebase/firestore";
+import {
+  addDoc,
+  doc,
+  setDoc,
+  collection,
+  getDoc,
+  getDocs,
+} from "firebase/firestore";
 
 function App() {
   const [titulo, setTitulo] = useState();
   const [autor, setAutor] = useState();
+  const [busca, setBusca] = useState([]);
 
   async function handleAdd() {
-    await setDoc(doc(db, "posts", "12345"), {
+    // await setDoc(doc(db, "posts", "12345"), {
+    //   titulo: titulo,
+    //   autor: autor,
+    // })
+    //   .then(() => {
+    //     console.log("DADOS REGISTRADO NO BANCO!");
+    //     setAutor('');
+    //     setTitulo('');
+    //   })
+    //   .catch((error) => {
+    //     alert("GEROU ERRO" + error);
+    //   });
+
+    // ADICIONANDO DOCUMENTO NO BANCO
+    await addDoc(collection(db, "posts"), {
       titulo: titulo,
       autor: autor,
     })
       .then(() => {
-        console.log("DADOS REGISTRADO NO BANCO!");
+        alert("CADASTRADO COM SUCESSO");
+        setAutor("");
+        setTitulo("");
       })
       .catch((error) => {
-        alert("GEROU ERRO" + error);
+        alert("ERRO " + error);
+      });
+  }
+
+  // Listando um POST ou VARIOS
+  async function handleSearch() {
+    // const postRef = doc(db, "posts", "123");
+    // await getDoc(postRef)
+    // .then((snapshot) => {
+    //   console.log(snapshot.data());
+    //   setBusca(snapshot.data());
+    // })
+    // .catch(() => {
+    //   console.log("ERRO AO BUSCAR");
+    // });
+
+    const postRef = collection(db, "posts");
+    await getDocs(postRef)
+      .then((snapshot) => {
+        let busca = [];
+        snapshot.forEach((doc) => {
+          busca.push({
+            id: doc.id,
+            titulo: doc.data().titulo,
+            autor: doc.data().autor,
+          });
+          console.log(busca);
+          setBusca(busca);
+        });
+      })
+      .catch((error) => {
+        console.log("ERRO AO BUSCAR" + error);
       });
   }
 
@@ -46,9 +101,25 @@ function App() {
           }}
         />
 
-        <button id="btnSubmit" onClick={handleAdd}>
+        <button className="btnSubmit" onClick={handleAdd}>
           Enviar
         </button>
+        <button className="btnSubmit" onClick={handleSearch}>
+          Buscar posts
+        </button>
+
+        <div className="resultContainer">
+          <ul>
+            {busca.map((item) => {
+              return (
+                <li key={item.id}>
+                  <span>Titulo: {item.titulo} </span> |
+                  <span> Autor: {item.autor}</span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
